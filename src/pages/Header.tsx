ede968f9c318e600c93b3ee3f4ec27b2e52f9c98
vijeyender <vijayvijeyender@gmail.com>
2023-10-06 06:16:24 +0530

    basic catogery page
    bash: line 1: wq: command not found

--- src/pages/Header.tsx
+++ src/pages/Header.tsx
@@ -3,7 +3,11 @@ function Header (){
         <section style={{"width": "100%"}}>
         <header>
         <h1 style={{"textAlign": "center"}}>MAX Market</h1>
+<<<<<<< HEAD
         <div className="container d-flex flex-row flex-grow-1 flex-shrink-1 justify-content-evenly"><input className="bg-secondary-subtle border rounded-pill border-primary-subtle shadow-lg d-flex flex-row flex-grow-1" type="search" placeholder="Search for Products, Brands and More"  autoComplete="on" style={{"paddingLeft": "17px"}} /><button className="btn btn-primary" type="submit">Search</button></div>
+=======
+        <div className="container d-flex flex-row flex-grow-1 flex-shrink-1 justify-content-evenly"><input className="bg-secondary-subtle border rounded-pill border-primary-subtle shadow-lg d-flex flex-row flex-grow-1" type="text" placeholder="Search for Products, Brands and More"  autoComplete="on" style={{"paddingLeft": "17px","marginRight":"24px"}} /><button className="btn btn-primary" type="submit">Search</button></div>
+>>>>>>> 83ee43e (basic catogery page)
     </header>
     </section>
     )
